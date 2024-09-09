c9261e072317a6c7a10ade7c3561542dc07e13f1
Eneas Baroni <eneasbaroni@gmail.com>
2024-09-08 23:13:02 -0300

    update about

--- src/app/(es)/about/page.tsx
+++ src/app/(es)/about/page.tsx
@@ -1,5 +1,11 @@
 'use client'
+import {Cormorant}  from "next/font/google"
 import { useState } from "react"
+
+const cormorant = Cormorant({ 
+    subsets: ["latin"], 
+    style: "italic",
+})
 
 const About = () => {
 
@@ -17,21 +23,24 @@ const About = () => {
                 <img src="./images/about/hst-01.svg" alt="icon" className="hstImg" />
                 <div className={`hst${hstActive === 0 ? ' hstActive' : ''}`}>
                     <h3>ARQU<span className="nautica">I</span>TECTUR<span className="nautica">A</span></h3>
-                    <p>SOY <span className="bigilla">ARQUITECTO</span>, RECIBIDO DE LA UNC. TENGO PASIÓN POR LA <span className="bigilla">ARQUITECTURA MINIMALISTA Y BRUTALISTA</span>. UN GRAN AMOR AL HORMIGÓN Y EL METAL</p>
-                </div> 
+                    <p>Soy <span className={cormorant.className}>Arquitecto</span>, recibido de la UNC. Tengo pasión por la <span className={cormorant.className}>arquitectura minimalista y brutalista</span>. Un gran amor por el hormigón y el metal. <br/>
+                    Como arquitecto, poseo una sólida comprensión de la estructura, el diseño espacial y la estética, lo que me permite construir <span className={cormorant.className}>interfaces de usuario intuitivas y visualmente atractivas.</span></p>
+                </div>  
             </div>
             <div className="history" onMouseEnter={() => setHstActive(1)} onMouseLeave={() => setHstActive(null)}>
                 <img src="./images/about/hst-02.svg" alt="icon" className="hstImg" />
                 <div className={`hst${hstActive === 1 ? ' hstActive' : ''}`}>
                     <h3>D<span className="nautica">I</span>SEÑO</h3>
-                    <p>DESDE CHICO FUI UN CURIOSO DEL <span className="bigilla">ARTE</span> Y PODER LLEVAR ESO HACIA EL <span className="bigilla">DISEÑO</span> FUE EL CAMINO QUE MARCO MI VIDA. EN LOS AÑOS DE FACULTAD HICE DIFERENTES CURSOS DE <span className="bigilla">DISEÑO GRÁFICO</span> QUE ME PERMITIERON ADENTRARME EN LA <span className="bigilla">RESOLUCIÓN DE PROBLEMAS</span> MEDIANTE EL DISEÑO.</p>
+                    <p>Desde chico fui un curioso del <span className={cormorant.className}>arte</span> y poder llevar eso hacia el <span className={cormorant.className}>diseño</span> fue el camino que marcó mi vida. En los años de facultad me capacité como <span className={cormorant.className}>diseñador gráfico.</span> Lo que me permitió adentrarme en la <span className={cormorant.className}>resolución de problemas</span> mediante el diseño.<br/>
+                    Mi formación en diseño gráfico me otorga una ventaja adicional para crear diseños <span className={cormorant.className}>web estéticamente agradables y funcionales.</span> 
+                    </p>
                 </div>                
             </div>
             <div className="history" onMouseEnter={() => setHstActive(2)} onMouseLeave={() => setHstActive(null)}>
                 <img src="./images/about/hst-03.svg" alt="icon" className="hstImg" />
                 <div className={`hst${hstActive === 2 ? ' hstActive' : ''}`}>
                     <h3>DES<span className="nautica">A</span>RROL<span className="nautica">L</span>O</h3>
-                    <p>DE LA UNIÓN DEL <span className="bigilla">ARTE, DISEÑO</span> Y LA CAPACIDAD DE PODER CREAR COSAS DESDE CERO ES QUE NACE ESTA NUEVA FACETA COMO <span className="bigilla">DESARROLLADOR WEB</span>. DONDE ME PERMITO CONJUGAR TODAS LAS EXPERIENCIAS Y APTITUDES ADQUIRIDAS.</p>
+                    <p>De la unión del <span className={cormorant.className}>arte, diseño</span> y la capcidad de poder crear cosas desde cero es que nace esta nueva faceta como <span className={cormorant.className}>Desarrollador Web</span>. Donde me permito conjugar todas las experiencias y aptitudes adquiridas.</p>
                 </div> 
             </div>
         </div>
