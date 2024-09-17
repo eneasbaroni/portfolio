3f647f196f5ac844609b29f7d8986e09bf2aff9f
Eneas Baroni <eneasbaroni@gmail.com>
2024-09-17 16:10:11 -0300

    mobile menu

--- src/app/_components/header/MobileMenu.tsx
+++ src/app/_components/header/MobileMenu.tsx
@@ -1,29 +1,54 @@
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React, { useState } from 'react'
+import { AnimatePresence, motion } from 'framer-motion';
+
+const anim = {
+    enter: {
+        y: ['-100vh'],
+        clipPath: ['ellipse(100% 100% at 50% 0%)'],
+    },
+
+    while: {
+        y: ['0vh'],
+        clipPath: ['ellipse(100% 117% at 50% 0%)'],
+        transition: {
+            duration: 1,
+            ease: [0.76, 0, 0.24, 1],
+            clipPath :{duration: 0.4, delay: 0.3, ease: [0.76, 0, 0.24, 1],}
+        }
+    },
+
+    exit: {
+        y: ['-100vh'],
+        clipPath: ['ellipse(120% 90% at 50% 0%)'],
+        transition: {
+            duration: 3,
+            ease: [0.76, 0, 0.24, 1],
+            clipPath :{duration: 0.4, ease: [0.76, 0, 0.24, 1],}
+        }
+    }
+}
+
+
 
 const MobileMenu = () => {
     const pathname = usePathname()
-    const [topPosition, setTopPosition] = useState("-100%")
-    const [navbarOpen, setNavbarOpen] = useState(false) 
-    
+    const [navbarOpen, setNavbarOpen] = useState(false)
+
     const open = () => {
         setNavbarOpen(true)
-        setTimeout(() => {            
-            setTopPosition("0%")
-        }, 1)
     }
 
     const close: () => void = () => {
-        setTimeout(() => {            
-            setTopPosition("-100%")
+        setTimeout(() => {
             setNavbarOpen(false)
         }, 800)
     }
 
     const handleMenu = () => {
         navbarOpen ? close() : open()
-    }    
+    }
 
     return (
         <header>
@@ -31,29 +56,37 @@ const MobileMenu = () => {
             <div className="menu">
                 <img className="menuIcon" width={25} src='/images/menu.svg' alt="menu" onClick={handleMenu} />
             </div>
+            <AnimatePresence>
 
-            {navbarOpen &&
-                <div className="mobileNavbar" style={{ top: topPosition }}>                    
-                    <div className='mobileHeader'>
-                        <h3>Eneas Baroni ®</h3>
-                        <h3 onClick={close}>X</h3>
-                    </div>
-                    {pathname.startsWith("/en") ?  
-                        <div className='mobileLinks'>
-                            <Link href="/en/" className="navItem" onClick={close}><span>H</span>OME</Link>
-                            <Link href="/en/about" className="navItem" onClick={close}>AB<span>O</span>UT</Link>
-                            <Link href="/en/works" className="navItem" onClick={close}><span>W</span>ORKS</Link>
-                            <Link href="/en/contact" className="navItem" onClick={close}>Contac<span>T</span></Link>
-                        </div>:
-                        <div className='mobileLinks'>
-                            <Link href="/" className="navItem" onClick={close}><span>I</span>nicio</Link>
-                            <Link href="/about" className="navItem" onClick={close}>Sobre <span>M</span>i</Link>
-                            <Link href="/works" className="navItem" onClick={close}><span>T</span>rabajos</Link>
-                            <Link href="/contact" className="navItem" onClick={close}>Contact<span>O</span></Link>
+                {navbarOpen &&
+                    <motion.div className="mobileNavbar"
+                        variants={anim}
+                        initial="enter"
+                        animate="while"
+                        exit='exit'
+                    >
+                        <div className='mobileHeader'>
+                            <h3>Eneas Baroni ®</h3>
+                            <h3 onClick={close}>X</h3>
                         </div>
-                    }
-                </div>                
-            }
+                        {pathname.startsWith("/en") ?
+                            <div className='mobileLinks'>
+                                <Link href="/en/" className="navItem" onClick={close}><span>H</span>OME</Link>
+                                <Link href="/en/about" className="navItem" onClick={close}>AB<span>O</span>UT</Link>
+                                <Link href="/en/works" className="navItem" onClick={close}><span>W</span>ORKS</Link>
+                                <Link href="/en/contact" className="navItem" onClick={close}>Contac<span>T</span></Link>
+                            </div> :
+                            <div className='mobileLinks'>
+                                <Link href="/" className="navItem" onClick={close}><span>I</span>nicio</Link>
+                                <Link href="/about" className="navItem" onClick={close}>Sobre <span>M</span>i</Link>
+                                <Link href="/works" className="navItem" onClick={close}><span>T</span>rabajos</Link>
+                                <Link href="/contact" className="navItem" onClick={close}>Contact<span>O</span></Link>
+                            </div>
+                        }
+                    </motion.div>
+                }
+            </AnimatePresence>
+
 
         </header>
     )
